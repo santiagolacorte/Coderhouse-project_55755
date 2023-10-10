7f9cfbe4e0d7c81dc0c974abc216ed5c84a82dcd
Santiago <selacorte@uc.cl>
2023-10-10 14:30:29 -0300

    ci: main-button and page title

--- src/static/js/parallax.js
+++ src/static/js/parallax.js
@@ -1,10 +1,20 @@
-let stars = getElementById('stars');
-let moon = getElementById('moon');
-let house = getElementById('house');
-let main_text = getElementById('main-text');
-let secondary_bg = getElementById('secondary-background');
-let main_button = getElementById('main-button');
-let main_bg = getElementById('main-background');
-let left_ghost = getElementById('left-ghost');
-let right_ghost = getElementById('right-ghost');
-let tombs = getElementById('tombs');
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let house = document.getElementById('house');
+let main_text = document.getElementById('main-text');
+let secondary_bg = document.getElementById('secondary-background');
+let main_button = document.getElementById('main-button');
+let left_ghost = document.getElementById('left-ghost');
+let right_ghost = document.getElementById('right-ghost');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    stars.style.left = value * 0.5 + 'px';
+    moon.style.left = value * 0.15 + 'px';
+    moon.style.top = value * 0.3 + 'px';
+    house.style.top = value * 0.15 + 'px';
+    secondary_bg.style.top = value * 0.15 + 'px';
+    main_button.style.marginTop = value + 'px';
+    left_ghost.style.left = value * 0.3 + 'px';
+    right_ghost.style.top = value * -0.5 + 'px';
+})
