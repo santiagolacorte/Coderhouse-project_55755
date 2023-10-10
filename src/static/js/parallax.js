27517c34f5775526ba452271e8b9f2401707002d
Santiago <selacorte@uc.cl>
2023-10-10 18:51:51 -0300

    ci: final stylizing of the webpage

--- src/static/js/parallax.js
+++ src/static/js/parallax.js
@@ -10,10 +10,10 @@ let right_ghost = document.getElementById('right-ghost');
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
     stars.style.left = value * 0.5 + 'px';
-    moon.style.left = value * 0.15 + 'px';
-    moon.style.top = value * 0.3 + 'px';
-    house.style.top = value * 0.15 + 'px';
-    secondary_bg.style.top = value * 0.15 + 'px';
+    moon.style.left = value * -0.35 + 'px';
+    moon.style.top = value * 0.35 + 'px';
+    // house.style.top = value * 0.175 + 'px';
+    secondary_bg.style.top = value * 0.2 + 'px';
     main_button.style.marginTop = value + 'px';
     left_ghost.style.left = value * 0.3 + 'px';
     right_ghost.style.top = value * -0.5 + 'px';
